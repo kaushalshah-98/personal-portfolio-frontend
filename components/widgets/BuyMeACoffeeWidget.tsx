6a258e9cdc39165e45129c881dbd1955f4336bd4
Mohammad Faisal <mohammadfaisal1011@gmail.com>
2022-01-19 22:43:14 +0600

    Added buy me a coffee button

--- components/widgets/BuyMeACoffeeWidget.tsx
+++ components/widgets/BuyMeACoffeeWidget.tsx
@@ -1,6 +1,5 @@
 const BuyMeACoffeeWidget = () => {
-  const path =
-    "https://www.buymeacoffee.com/widget/page/mohammadfaisal?description=Support%20me%20on%20Buy%20me%20a%20coffee!&color=%235F7FFF";
+  const path = `https://www.buymeacoffee.com/widget/page/${process.env.NEXT_PUBLIC_BUY_ME_A_COFFEE_USERNAME}?description=Support%20me%20on%20Buy%20me%20a%20coffee!&color=%235F7FFF`;
 
   return (
     <div className="flex justify-center rounded" style={{ height: "700px" }}>
