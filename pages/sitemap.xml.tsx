e96ef8eb9cf7035b812b41d235cf1ba2ddc0d1b0
Mohammad Faisal <mohammadfaisal1011@gmail.com>
2022-02-05 23:05:23 +0600

    resolved error

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -6,31 +6,30 @@ const Sitemap = () => {
   return null;
 };
 
-export const getServerSideProps = async ({
-  res,
-}: GetServerSidePropsContext): Promise<GetServerSidePropsResult<Record<string, string>>> => {
-  const BASE_URL =
-    process.env.NODE_ENV === "production" ? process.env.NEXT_PUBLIC_PRODUCTION_ROOT_URL : "http://localhost:3000";
+export const getServerSideProps = async ({ res }: GetServerSidePropsContext): Promise<any> => {
+  try {
+    const BASE_URL =
+      process.env.NODE_ENV === "production" ? process.env.NEXT_PUBLIC_PRODUCTION_ROOT_URL : "http://localhost:3000";
 
-  const otherPaths: string[] = fs
-    .readdirSync("pages")
-    .filter((staticPage) => {
-      return !["api", "posts", "index.tsx", "_app.tsx", "_document.tsx", "404.tsx", "sitemap.xml.tsx"].includes(
-        staticPage
-      );
-    })
-    .map((staticPagePath) => {
-      return `${BASE_URL}/${staticPagePath}`;
-    });
+    const otherPaths: string[] = fs
+      .readdirSync("pages")
+      .filter((staticPage) => {
+        return !["api", "posts", "index.tsx", "_app.tsx", "_document.tsx", "404.tsx", "sitemap.xml.tsx"].includes(
+          staticPage
+        );
+      })
+      .map((staticPagePath) => {
+        return `${BASE_URL}/${staticPagePath}`;
+      });
 
-  const postSlugs = getPostSlugs();
-  const postPaths = postSlugs.map((slug) => `${BASE_URL}/blog/${slug.replace(".md", "")}`);
+    const postSlugs = getPostSlugs();
+    const postPaths = postSlugs.map((slug) => `${BASE_URL}/blog/${slug.replace(".md", "")}`);
 
-  const allPaths = [...postPaths, ...otherPaths];
+    const allPaths = [...postPaths, ...otherPaths];
 
-  // const allPaths = ["https://mohammadfaisal.dev/blog/how-to-create-a-sitemap"];
+    // const allPaths = ["https://mohammadfaisal.dev/blog/how-to-create-a-sitemap"];
 
-  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+    const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     ${allPaths
       .map((url) => {
@@ -47,13 +46,16 @@ export const getServerSideProps = async ({
     </urlset>
   `;
 
-  res.setHeader("Content-Type", "text/xml");
-  res.write(sitemap);
-  res.end();
+    res.setHeader("Content-Type", "text/xml");
+    res.write(sitemap);
+    res.end();
 
-  return {
-    props: {},
-  };
+    return {
+      props: {},
+    };
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 export default Sitemap;
